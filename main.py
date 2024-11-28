a71c31fb086095a4ddb8d5e65c43325e57c0bbf3
Jurand76 <jurand@interia.pl>
2024-11-29 00:21:26 +0100

    reading messages from channel - added functionality

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from secrets import getDiscordToken
 
 # Inicjalizacja klienta bota
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True  # Pozwala odczytywać treść wiadomości na serwerze
 
+"""
+Aby bot czytał wiadomości z kanału - w Discord Developer Portal:
+- przejdź do zakładki Bot
+- w sekcji Privileged Gateway Intents włącz Message Content Intent.
+"""
+
+# Przekaż intents do klienta
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
